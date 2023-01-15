9a9cb3530b8ec13f08712bb0b4319d9ba7e54f41
Bisri Musthofa <bisri@eannovate.com>
2023-01-15 23:36:21 +0700

    build 23:36

---
+++ public/build/assets/SpinnerProcessing.c1fd1dc9.js
@@ -0,0 +1,1 @@
+import{_ as n,o as r,f as s,n as t}from"./app.b8fedaf9.js";const o={props:{me:{type:String,default:"me-5"},color:{type:String,default:"text-success"},size:{type:String,default:""}}};function c(a,i,e,l,p,d){return r(),s("div",{class:t(`spinner-border ${e.color} ${e.me} spinner-border-${e.size}`)},null,2)}const _=n(o,[["render",c]]);export{_ as S};
