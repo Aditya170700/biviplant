256529765e7408333490238d85568672925d7697
adityaricki <52729836+Aditya170700@users.noreply.github.com>
2023-01-18 00:02:03 +0700

    Bug nextTick

--- resources/js/Pages/Chat.vue
+++ resources/js/Pages/Chat.vue
@@ -1,50 +1,60 @@
 <script setup>
-    import { nextTick, onMounted, onUpdated, reactive, ref, watch } from "vue";
-    import HeaderWithTitle from '../Shared/HeaderWithTitle.vue';
-    import { Head } from "@inertiajs/inertia-vue3";
-    import Sidebar from "./../Shared/Homepage/Sidebar.vue";
-    import socket from '../socket.js';
+import { nextTick, onMounted, onUpdated, reactive, ref, watch } from "vue";
+import HeaderWithTitle from "../Shared/HeaderWithTitle.vue";
+import { Head } from "@inertiajs/inertia-vue3";
+import Sidebar from "./../Shared/Homepage/Sidebar.vue";
+import socket from "../socket.js";
 
-    const props = defineProps({
-        meta_title: String,
-        meta_description: String,
-        meta_keyword: String,
-        user: Object,
-        conversation: Object
-    })
-    let textMessage = ref('')
-    let messages = ref([])
+const props = defineProps({
+    meta_title: String,
+    meta_description: String,
+    meta_keyword: String,
+    user: Object,
+    conversation: Object,
+});
+let textMessage = ref("");
+let messages = ref([]);
 
-    messages.value = props.conversation.messages
-    
-    /**
-     * SOCKET EVENT
-     */
-    socket.on('message', (data) => {
-        messages.value.push(data)
-        nextTick(() => {
-            window.scrollTo(0, window.document.getElementsByClassName("page-content-wrapper")[0].scrollHeight)
-        })
-    })
-    
-    function submit(e) {
-        e.preventDefault()
-        if (textMessage.value != '') {
-            socket.emit("message-to-admin", {
-                user_id: props.user.id,
-                message: textMessage.value,
-                conversation_id: props.conversation.id
-            })
-            textMessage.value = ''
+messages.value = props.conversation.messages;
+
+/**
+ * SOCKET EVENT
+ */
+socket.on("message", (data) => {
+    messages.value.push(data);
+    nextTick(() => {
+        if (route().current() == "chat.index") {
+            window.scrollTo(
+                0,
+                window.document.getElementsByClassName(
+                    "page-content-wrapper"
+                )[0].scrollHeight
+            );
         }
+    });
+});
+
+function submit(e) {
+    e.preventDefault();
+    if (textMessage.value != "") {
+        socket.emit("message-to-admin", {
+            user_id: props.user.id,
+            message: textMessage.value,
+            conversation_id: props.conversation.id,
+        });
+        textMessage.value = "";
     }
+}
 
-    onMounted(() => {
-        nextTick(() => {
-            window.scrollTo(0, window.document.getElementsByClassName("page-content-wrapper")[0].scrollHeight)
-        })
-    })
-
+onMounted(() => {
+    nextTick(() => {
+        window.scrollTo(
+            0,
+            window.document.getElementsByClassName("page-content-wrapper")[0]
+                .scrollHeight
+        );
+    });
+});
 </script>
 
 <template>
@@ -62,7 +72,10 @@
         <Sidebar></Sidebar>
         <div class="page-content-wrapper">
             <!-- Live Chat Intro-->
-            <div class="live-chat-intro mb-3" style="position: fixed; width: 100%; z-index: 1000;">
+            <div
+                class="live-chat-intro mb-3"
+                style="position: fixed; width: 100%; z-index: 1000"
+            >
                 <p>Mulai Percakapan Dengan Admin</p>
                 <img src="/assets/images/customer-service.png" alt="" />
                 <div class="status online">Admin sedang online</div>
@@ -70,13 +83,22 @@
                 <!-- .status.offline We’ll be back soon-->
             </div>
             <!-- Support Wrapper-->
-            <div class="support-wrapper py-3" style="padding-top: 200px !important;">
+            <div
+                class="support-wrapper py-3"
+                style="padding-top: 200px !important"
+            >
                 <div class="container">
                     <!-- Live Chat Wrapper-->
-                    <div class="live-chat-wrapper" v-for="(message, i) in messages" :key="i">
-                        
+                    <div
+                        class="live-chat-wrapper"
+                        v-for="(message, i) in messages"
+                        :key="i"
+                    >
                         <!-- Agent Message Content-->
-                        <div class="agent-message-content d-flex align-items-start" v-if="message.user.role == 'admin'">
+                        <div
+                            class="agent-message-content d-flex align-items-start"
+                            v-if="message.user.role == 'admin'"
+                        >
                             <!-- <div class="agent-thumbnail me-2 mt-3">
                                 <img src="/img/bg-img/9.jpg" alt="" />
                             </div> -->
@@ -85,7 +107,9 @@
                                     <p>
                                         {{ message.message }}
                                     </p>
-                                    <span style="font-size: 10px;">{{ message.date_time_formated }}</span>
+                                    <span style="font-size: 10px">{{
+                                        message.date_time_formated
+                                    }}</span>
                                 </div>
                             </div>
                         </div>
@@ -94,11 +118,18 @@
                         <div class="user-message-content" v-else>
                             <div class="user-message-text">
                                 <div class="d-block mt-2 mt-3">
-                                    <p style="background-color: #FFFFFF !important; color: #747794;">
+                                    <p
+                                        style="
+                                            background-color: #ffffff !important;
+                                            color: #747794;
+                                        "
+                                    >
                                         {{ message.message }}
                                     </p>
                                 </div>
-                                <span style="font-size: 10px;">{{ message.date_time_formated }}</span>
+                                <span style="font-size: 10px">{{
+                                    message.date_time_formated
+                                }}</span>
                             </div>
                         </div>
                     </div>
@@ -117,7 +148,11 @@
                     placeholder="Tulis pesan"
                 ></textarea>
                 <button type="submit" @click="submit">
-                    <img src="/assets/images/send-message.png" alt="" style="opacity: 0.5;">
+                    <img
+                        src="/assets/images/send-message.png"
+                        alt=""
+                        style="opacity: 0.5"
+                    />
                 </button>
             </form>
         </div>
